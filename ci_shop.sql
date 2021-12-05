d67f3d332ec7ccdee8eb3cb31533b060cf4b15a8
sodiqmahfud <sodiq.mahfud@gmail.com>
2021-12-05 19:13:59 +0700

    upload database

--- ci_shop.sql
+++ ci_shop.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 09, 2020 at 04:37 AM
--- Server version: 5.7.24
--- PHP Version: 7.4.3
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2021 at 01:12 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -90,9 +89,7 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `id_user`, `date`, `invoice`, `total`, `name`, `address`, `phone`, `status`) VALUES
-(1, 5, '2020-03-18', '520200318210456', 36000000, 'Hakim', 'Kampung Malang Kulon 1/38-A', '087855777360', 'paid'),
-(2, 5, '2020-03-19', '520200319181238', 500000, 'Jotaro Kujo', 'Western', '218838383', 'delivered'),
-(3, 5, '2020-03-24', '520200324223408', 3000000, 'Amir Muhammad Hakim', 'Kampung Malang Kulon 1/38-A', '087855777360', 'waiting');
+(4, 9, '2021-12-05', '920211205183753', 3000000, 'customer', 'jl customer membeli ps', '08111111111', 'paid');
 
 -- --------------------------------------------------------
 
@@ -115,8 +112,7 @@ CREATE TABLE `orders_confirm` (
 --
 
 INSERT INTO `orders_confirm` (`id`, `id_orders`, `account_name`, `account_number`, `nominal`, `note`, `image`) VALUES
-(1, 1, 'Amir', '42424123333', 36000000, '-', '520200318210456-20200319173859.jpg'),
-(2, 2, 'Dio Brando', '344312321', 500000, 'Mantap kang', '520200319181238-20200319181447.jpg');
+(3, 4, 'customer', '0981211', 3000000, '-bayar ps', '920211205183753-20211205183905.jpg');
 
 -- --------------------------------------------------------
 
@@ -140,7 +136,8 @@ INSERT INTO `order_detail` (`id`, `id_orders`, `id_product`, `qty`, `subtotal`) 
 (1, 1, 4, 6, 30000000),
 (2, 1, 3, 2, 6000000),
 (3, 2, 2, 1, 500000),
-(4, 3, 3, 1, 3000000);
+(4, 3, 3, 1, 3000000),
+(5, 4, 3, 1, 3000000);
 
 -- --------------------------------------------------------
 
@@ -155,7 +152,7 @@ CREATE TABLE `product` (
   `title` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
-  `is_available` tinyint(1) NOT NULL DEFAULT '1',
+  `is_available` tinyint(1) NOT NULL DEFAULT 1,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -166,7 +163,9 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`id`, `id_category`, `slug`, `title`, `description`, `price`, `is_available`, `image`) VALUES
 (2, 3, 'ps-3-stick-console', 'PS 3 Stick Console', 'Stick controller for PS 3', 500000, 1, 'console.jpg'),
 (3, 3, 'playstation-3', 'Playstation 3', 'PS 3 new edition', 3000000, 1, 'playstation-3-20200314173701.jpg'),
-(4, 2, 'amd-ryzen-5', 'AMD Ryzen 5', 'Generasi terbaru prosesor AMD', 5000000, 1, 'amd-ryzen-5-20200314174154.jpg');
+(4, 2, 'amd-ryzen-5', 'AMD Ryzen 5', 'Generasi terbaru prosesor AMD', 5000000, 1, 'amd-ryzen-5-20200314174154.jpg'),
+(5, 3, 'playstation-5', 'Playstation 5', 'Sony Playstation 5', 6500000, 1, 'playstation-5-20211205185532.jpg'),
+(6, 5, 'iphone-11-pro', 'Iphone 11 Pro', 'Iphone 11 Pro 256G', 13500000, 1, 'iphone-11-pro-20211205185755.jpg');
 
 -- --------------------------------------------------------
 
@@ -180,7 +179,7 @@ CREATE TABLE `user` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('admin','member') NOT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
   `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -190,7 +189,10 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `role`, `is_active`, `image`) VALUES
 (4, 'Admin', 'admin@admin.com', '$2y$10$mLyToNOtVeLG.ziTUFPpCOOGD1P8cXZJj6ufU7J74TB63qVs2JjqK', 'admin', 1, 'admin-20200315212825.png'),
-(5, 'Member', 'member@member.com', '$2y$10$ghbQkKwENFZnOxWAwtgSaeDV2gcI63ZorQEQkSxqlaUlrFUMNnLpy', 'member', 1, 'member-20200315232137.png');
+(5, 'Member', 'member@member.com', '$2y$10$ghbQkKwENFZnOxWAwtgSaeDV2gcI63ZorQEQkSxqlaUlrFUMNnLpy', 'member', 1, 'member-20200315232137.png'),
+(7, 'administrator', 'admin.shop@cishop.com', '$2y$10$vU2WpnyzCVOA6VWtv5Dya.7lnt7ozJWa9utvToUIlCue8VHftuf5S', 'admin', 1, 'administrator-20211129232928.jpg'),
+(8, 'administrator', 'administrator@adminshop.com', '$2y$10$Nm5I7I86k1Arn9GnBxag2erZDscRWlHHhcCFCF8HppcuE54ORpLWi', 'admin', 1, NULL),
+(9, 'customer', 'customer@buy.com', '$2y$10$ixiapSCP3Ra5E64CTlc1XebHtlwsg1MkYGpeFUi8oWTt0MpTsFNE2', 'member', 1, NULL);
 
 --
 -- Indexes for dumped tables
@@ -246,7 +248,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -258,31 +260,31 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `orders_confirm`
 --
 ALTER TABLE `orders_confirm`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `order_detail`
 --
 ALTER TABLE `order_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
